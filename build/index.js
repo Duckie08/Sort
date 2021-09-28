4c4f57970da95ff4cb07181fd3594c143606ec7b
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-09-28 15:40:42 +0700

    feat: create numberCollection and sorter classes

--- build/index.js
+++ build/index.js
@@ -1,1 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sorter_1 = require("./sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numberCollection = new NumbersCollection_1.NumbersCollection([-10, 3, -5, 0]);
+var sorter = new sorter_1.Sorter(numberCollection);
+sorter.sort();
+console.log(numberCollection.data);
