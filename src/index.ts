4ef09d4fb6403f06dc33a6fabfb99e3113fed282
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-02 00:52:42 +0700

    feat: implementing linked list

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersColelction } from "./CharactersCollection";
 
-const numberCollection = new NumbersCollection([-10, 3, -5, 0]);
-const sorter = new Sorter(numberCollection);
+// const numberCollection = new NumbersCollection([-10, 3, -5, 0]);
+// const sorter = new Sorter(numberCollection);
+// sorter.sort();
+// console.log(numberCollection.data)
+
+const charactiersCollection = new CharactersColelction('cba');
+const sorter = new Sorter(charactiersCollection);
 sorter.sort();
-console.log(numberCollection.data)
+console.log(charactiersCollection.data);
